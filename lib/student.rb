e92ee220f059e22223ac52d63e2a069e4cd21090
DJ Taylor <dtaylor88@gmail.com>
2021-06-10 05:01:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,18 @@
 require_relative './user.rb'
 
-class Student < User 
+class Student < User
+  attr_accessor :knowledge 
+  
+  def initialize
+    @knowledge = []
+  end 
 
+  def learn(string)
+    @knowledge << string 
+  end 
+  
+  def knowledge
+    @knowledge
+  end 
+  
 end
